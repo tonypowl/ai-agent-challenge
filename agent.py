571c2337d5814028a6240b345f450ab49085c0ad
tonypowl <tonypaul0412@gmail.com>
2025-10-04 21:25:29 +0530

    worflow structure mentioned as comments

---
+++ agent.py
@@ -0,0 +1,5 @@
+#take input -> --target _bank_name_ (in this case the icici files)
+#check the files in data folder (.pdf) and generates a icic_parser.py with a parse() function 
+#import the new parse funtion run it on the .pdf and compare the new csv with the old csv 
+#if inaccurate then retry atmost of 3 times
+
